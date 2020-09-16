1a7b46c71a19f86aecd4df5deb90af9668a1ab14
Damon Jellema <damon.jellema2004@gmail.com>
2020-09-16 12:21:11 +0200

    PoemDamonJellema

---
+++ poem.py
@@ -0,0 +1,9 @@
+verse1 = "Hallo ik ben Damon Jellema.Ik ben 15 jaar en zit op het Ma." 
+verse2 = "Ik woon in een dorpje genaamd Heemskerk. Dat is ook de plaats waar ik werk."
+verse3 = "Mijn hobbys zijn schieten, gamedevelopen, Krav maga en Gamen. Daar valt nog wat meer over te weten."
+verse4 = "Mijn favoriete game momenteel is League of legends. En een van mijn favoriete film is Star wars the phantom menace." 
+
+print(verse1)
+print(verse2)
+print(verse3)
+print(verse4)
