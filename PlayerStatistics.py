e3f04aa2073d190feb6b2d6650b4317cd750fb7b
Damon Jellema <damon.jellema2004@gmail.com>
2020-09-23 12:33:57 +0200

    Stats

---
+++ PlayerStatistics.py
@@ -0,0 +1,30 @@
+Name = "Name: Damon"
+Level = 1
+Class = "Class: Heavy"
+Speed = 55.5
+Strength = "Strength: 200"
+PrimaryWeapon = "PrimaryWeapon: Axe"
+SecondaryWapon = "SecondaryWapon: Shield"
+Stamina = "Stamina: 150"
+ElementType = "ElementType: Lightning"
+Ability = "Ability: LightningSlash"
+
+
+
+
+Level+=6
+
+Speed *=4
+
+
+
+print(Name)
+print("Level: "+ str(Level))
+print(Class)
+print("Speed: "+ str(Speed))
+print(Strength)
+print(PrimaryWeapon)
+print(SecondaryWapon)
+print(Stamina)
+print(ElementType)
+print(Ability)
