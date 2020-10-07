bb3f32c5227ea987e12341c24c29d02ed37374de
Damon Jellema <damon.jellema2004@gmail.com>
2020-10-07 12:04:45 +0200

    MyFirstOperation

---
+++ MyFirstOperation.py
@@ -0,0 +1,20 @@
+Name = "Damon"
+Leeftijd = 16 + 2
+Geboorte = 2004 / 2
+Klas = 1 * 2
+Hobby = 5 - 5
+Modulo = 30 % 1
+
+Leeftijd-=3
+Geboorte*=2
+Klas/=2
+Hobby+=3
+Modulo%=7
+
+print(Name + " " + "Jellema")
+print("Leeftijd: "+ str(Leeftijd))
+print("Geboorte: "+ str(Geboorte))
+print("Klas: "+ str(Klas))
+print("Hobbys: Krav Maga, Schieten, Gamen.")
+print("Hobby: "+ str(Hobby))
+print("Modulo: "+ str(Modulo))
